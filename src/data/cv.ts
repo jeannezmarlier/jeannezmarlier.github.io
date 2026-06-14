cf91bfd99bb9b72f246e7713396eb684e0af60e4
Jeanne Marlier <83042644+jeannezmarlier@users.noreply.github.com>
2026-06-14 17:12:58 +0200

    Update cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -45,12 +45,12 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
+		title: 'Why do people like technocrats?',
+		authors: 'Jeanne Marlier, Matthias Kaltenegger, Laurenz Ennser-Jedenastik',
+		journal: 'European Journal of Political Research',
+		time: '2025',
+		link: 'https://doi.org/10.1111/1475-6765.12709 ',
+		abstract: 'Many voters support the inclusion of technocrats in government. Yet we know very little about why technocrats are considered more appealing than traditional party representatives. In particular, it is unclear which advantages and disadvantages voters attach to the defining traits of technocratic ministers: party independence and expertise. We engage with this question drawing on a pre-registered survey experiment in Austria. We examine how manipulating ministers' party affiliation and expertise affects voters' perceptions of their issue competence and bargaining competence. Findings indicate that voters ascribe lower levels of issue competence to partisan ministers than to non-partisan ministers, notwithstanding their actual expertise, and that ministers' partisanship shrinks the positive effect of expertise on perceived issue competence. However, this ‘partisanship penalty’ disappears for supporters of the minister's party. Moreover, voters perceive partisanship as an advantageous trait with regard to a minister's bargaining competence. While voters like technocrats for their expertise and independence from party politics, our findings reveal nuanced perceptions, with voters still recognizing distinct advantages in being represented by party politicians.',
 	},
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',
